4725fabae20c7b628b62cab5b42c82434c45c9ea
nmarshall <marshall.nic@gmail.com>
2010-08-27 18:31:49 +0100

    Added rutils from lsbardel

--- utils/djutils.py
+++ utils/djutils.py
@@ -22,6 +22,9 @@ def django_choices(vals):
 
 try:
     from django.db import models
+    from django import forms
+    import json
+    
     class SlugCode(models.CharField):
         
         def __init__(self, rtxchar='_', lower=False, upper = False, **kwargs):
@@ -43,6 +46,44 @@ try:
             value = self.trim(value)
             setattr(model_instance, self.attname, value)
             return value
+    
+    class JSONWidget(forms.Textarea):
+        def render(self, name, value, attrs=None):
+            if not isinstance(value, basestring):
+                value = json.dumps(value, indent=2)
+            return super(JSONWidget, self).render(name, value, attrs)
+ 
+    class JSONFormField(forms.CharField):
+        def __init__(self, *args, **kwargs):
+            kwargs['widget'] = JSONWidget
+            super(JSONFormField, self).__init__(*args, **kwargs)
+ 
+        def clean(self, value):
+            if not value: return
+            try:
+                return json.loads(value)
+            except Exception, exc:
+                raise forms.ValidationError(u'JSON decode error: %s' % (unicode(exc),))
+ 
+    class JSONField(models.TextField):
+        __metaclass__ = models.SubfieldBase
+ 
+        def formfield(self, **kwargs):
+            return super(JSONField, self).formfield(form_class=JSONFormField, **kwargs)
+ 
+        def to_python(self, value):
+            if isinstance(value, basestring) and value:
+                value = json.loads(value)
+            return value
+ 
+        def get_db_prep_save(self, value):
+            if value is None: return
+            return json.dumps(value)
+ 
+        def value_to_string(self, obj):
+            value = self._get_val_from_obj(obj)
+            return self.get_db_prep_value(value)
+    
 except Exception, e:
     print e
     
